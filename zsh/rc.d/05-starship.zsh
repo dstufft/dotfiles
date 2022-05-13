ebb29b635e31211ed8ee8d89bdd1367a2c05e405
Donald Stufft <donald@stufft.io>
2022-05-13 15:39:51 -0400

    Don't auto install starship

--- zsh/rc.d/05-starship.zsh
+++ zsh/rc.d/05-starship.zsh
@@ -3,13 +3,6 @@
 # Setup where our starship config can be found at
 export STARSHIP_CONFIG="$settingsdir/starship/starship.toml"
 
-# Download starship, if it's not there yet.
-if [ ! -f $HOME/.local/bin/starship ]; then
-    mkdir -p ~/.local/bin
-    curl -sS https://starship.rs/install.sh | sh -s -- --bin-dir ~/.local/bin --yes
-    rehash
-fi
-
 # Setup our prompt to use starship
 znap eval starship 'starship init zsh --print-full-init'
 znap prompt

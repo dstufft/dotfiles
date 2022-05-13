6b2367ee10163845908b240ce0061ba7e96acd31
Donald Stufft <donald@stufft.io>
2022-05-13 12:21:53 -0400

    Use exa instead of ls

--- zsh/rc.d/06-plugins.zsh
+++ zsh/rc.d/06-plugins.zsh
@@ -7,7 +7,8 @@
 # For more info on each plugin, visit its repo at github.com/<plugin>
 # -a sets the variable's type to array.
 local -a plugins=(
-    ellie/atuin  # Replace history with an SQLite database w/ syncing
+    ellie/atuin                   # Replace history with an SQLite database w/ syncing
+    birdhackor/zsh-exa-ls-plugin  # Use exa instead of ls and tree
 )
 
 

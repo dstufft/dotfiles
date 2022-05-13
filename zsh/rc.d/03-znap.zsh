e7853e819a48f54f611599f1866c5195456d0038
Donald Stufft <donald@stufft.io>
2022-05-13 11:42:55 -0400

    Create a directory for zsh repos

--- zsh/rc.d/03-znap.zsh
+++ zsh/rc.d/03-znap.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-local znap=$settingsdir/zsh/zsh-snap/znap.zsh
+local znap=$settingsdir/zsh/repos/zsh-snap/znap.zsh
 
 # Download Znap, if it's not there yet.
 if ! [[ -r $znap ]]; then
@@ -10,5 +10,5 @@ if ! [[ -r $znap ]]; then
         https://github.com/marlonrichert/zsh-snap.git $znapdir
 fi
 
-# zstyle ':znap:*' repos-dir ~/.config/zsh
+zstyle ':znap:*' repos-dir $settingsdir/zsh/repos
 . $znap     # Load Znap.

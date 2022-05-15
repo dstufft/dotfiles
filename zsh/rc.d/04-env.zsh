b441d01e8eacc2ddc3cba972f3a5e0d3a9413412
Donald Stufft <donald@stufft.io>
2022-05-15 15:16:11 -0400

    Add Homebrew completions to our FPATH

--- zsh/rc.d/04-env.zsh
+++ zsh/rc.d/04-env.zsh
@@ -9,7 +9,7 @@ if grep -q microsoft /proc/sys/kernel/osrelease; then
     local platform="windows-wsl"
 fi
 
-# Setup our PATH
+# Setup our PATH, and FPATH
 # $PATH and $path (and also $FPATH and $fpath, etc.) are "tied" to each other.
 # Modifying one will also modify the other.
 # Note that each value in an array is expanded separately. Thus, we can use ~
@@ -20,6 +20,12 @@ path=(
     $path
 )
 
+fpath=(
+    ~/.linuxbrew/share/zsh/site-functions
+    $fpath
+)
+
+
 # Add any platform specific paths
 if [[ $platform == "windows-wsl" ]]; then
     path+=( /mnt/c/WSLBin )
@@ -27,3 +33,4 @@ fi
 
 # -U ensures each entry in these is Unique (that is, discards duplicates).
 export -U PATH path
+export -U FPATH fpath

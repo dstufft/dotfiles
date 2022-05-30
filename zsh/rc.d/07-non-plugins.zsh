b18866dd61e8105ecd784fdd961c0648dd339d66
Donald Stufft <donald@stufft.io>
2022-05-30 16:40:16 -0400

    Setup a few non plugins to work consistently

--- zsh/rc.d/07-non-plugins.zsh
+++ zsh/rc.d/07-non-plugins.zsh
@@ -2,3 +2,15 @@
 ##
 # Non Plugins
 #
+
+# direnv
+znap eval direnv 'direnv hook zsh'
+
+# pipx Python
+znap eval pipx 'echo export PIPX_DEFAULT_PYTHON=$(asdf which python)'
+
+# aws-vault
+if [[ $platform == "windows-wsl" ]]; then
+    export AWS_VAULT_BACKEND=pass
+    export GPG_TTY=$(tty)
+fi

5a2bc2fa55bbe6edcfc4180acc8ba702d873b1b1
Donald Stufft <donald@stufft.io>
2022-05-30 16:41:00 -0400

    Use the default linuxbrew directory

--- zsh/rc.d/04-env.zsh
+++ zsh/rc.d/04-env.zsh
@@ -9,6 +9,12 @@ if grep -q microsoft /proc/sys/kernel/osrelease; then
     local platform="windows-wsl"
 fi
 
+# Setup our Homebrew environments
+export HOMEBREW_BUNDLE_FILE="$HOME/.config/Brewfile"
+export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
+export HOMEBREW_CELLAR="/home/linuxbrew/.linuxbrew/Cellar"
+export HOMEBREW_REPOSITORY="/home/linuxbrew/.linuxbrew/Homebrew"
+
 # Setup our PATH, and FPATH
 # $PATH and $path (and also $FPATH and $fpath, etc.) are "tied" to each other.
 # Modifying one will also modify the other.
@@ -16,13 +22,29 @@ fi
 # for $HOME in each $path entry.
 path=(
     ~/.local/bin
+    ~/.linuxbrew/sbin
     ~/.linuxbrew/bin
+    /home/linuxbrew/.linuxbrew/bin
+    /home/linuxbrew/.linuxbrew/sbin
     $path
 )
 
 fpath=(
     ~/.linuxbrew/share/zsh/site-functions
+    /home/linuxbrew/.linuxbrew/share/zsh/site-functions
     $fpath
+)
+
+manpath=(
+    ~/.linuxbrew/share/man
+    /home/linuxbrew/.linuxbrew/share/man
+    $manpath
+)
+
+infopath=(
+    ~/.linuxbrew/share/info
+    /home/linuxbrew/.linuxbrew/share/info
+    $infopath
 )
 
 
@@ -34,3 +56,5 @@ fi
 # -U ensures each entry in these is Unique (that is, discards duplicates).
 export -U PATH path
 export -U FPATH fpath
+export -U MANPATH manpath
+export -U INFOPATH infopath

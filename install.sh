727745e6c7bbb4337f1afb23ac3dba6b8fa18186
Donald Stufft <donald.stufft@datadoghq.com>
2024-03-20 22:26:54 -0400

    Install Homebrew

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@ set -euxo pipefail
 
 DOTFILES_DIR="$(dirname "$(readlink -f "$0")")"
 
+# Setup zsh
 echo "DOTFILES_DIR=$DOTFILES_DIR" > ~/.zshenv
 echo '. $DOTFILES_DIR/zsh/.zshenv' >> ~/.zshenv
+
+# Install Homebrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"

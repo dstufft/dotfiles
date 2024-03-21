48fbe62752f4e97f408b2a1e94784077b19fb186
Donald Stufft <donald.stufft@datadoghq.com>
2024-03-20 22:39:35 -0400

    comment

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ echo "DOTFILES_DIR=$DOTFILES_DIR" > ~/.zshenv
 echo '. $DOTFILES_DIR/zsh/.zshenv' >> ~/.zshenv
 
 # Install Homebrew
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"

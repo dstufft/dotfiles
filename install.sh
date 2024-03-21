af73eaad5b724994ddfe0ae9082245b6f0c829b3
Donald Stufft <donald.stufft@datadoghq.com>
2024-03-20 22:16:51 -0400

    set env vars in .zshenv

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-set -euxo pipefile
+set -euxo pipefail
+
+DOTFILES_DIR="$(dirname "$(readlink -f "$0")")"
+
+echo "DOTFILES_DIR=$DOTFILES_DIR" > ~/.zshenv
+echo '. $DOTFILES_DIR/zsh/.zshenv' >> ~/.zshenv
